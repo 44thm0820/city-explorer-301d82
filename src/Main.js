d13e734dfdfca841829ce9036f7f0f407c3ec0eb
Rey Mercado <reymercado.usa@gmail.com>
2022-02-21 15:42:09 -0800

    baseline create-react-app for 301

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <header>
+        <h2>What's up main</h2>
+      </header>
+    )
+  }
+}
+
+export default Main;
