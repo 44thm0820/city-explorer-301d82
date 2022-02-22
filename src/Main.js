2cd7887be8b4508c6d25e7594fd39f8617fca27b
Rey Mercado <reymercado.usa@gmail.com>
2022-02-21 19:42:15 -0800

    submit city with axios - wip

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Main extends React.Component {
   constructor(props){
@@ -14,6 +15,16 @@ class Main extends React.Component {
       city: e.target.value
     });
   };
+
+  getCityData = async (e) => {
+    e.preventDefault();
+
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+
+    let cityData = await axios.get(url);
+    
+    console.log(cityData.data[0]);
+  }
 
   render() {
     console.log('app state: ', this.state);
